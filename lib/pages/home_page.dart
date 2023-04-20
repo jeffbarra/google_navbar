a7876e2dbf012a65873591c892a750ce02e39450
Jeff Barra <103481560+jeffbarra@users.noreply.github.com>
2023-04-19 18:19:28 -0700

    Add files via upload

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        color: Colors.red.shade400,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: GNav(
+              backgroundColor: Colors.red.shade400,
+              color: Colors.white,
+              activeColor: Colors.black,
+              tabBackgroundColor: Colors.white,
+              gap: 8,
+              padding: EdgeInsets.all(16),
+              tabs: [
+                GButton(
+                  icon: Icons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: Icons.favorite,
+                  text: 'Likes',
+                ),
+                GButton(
+                  icon: Icons.search,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: Icons.settings,
+                  text: 'Settings',
+                ),
+              ]),
+        ),
+      ),
+    );
+  }
+}
